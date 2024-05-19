79ca12513f192345b0556aa8b25f1ce7dc2fbe1e
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2024-05-19 22:38:11 +0200

    Create a class `TaskManager` to manage all the tasks.

---
+++ src/TaskManager.java
@@ -0,0 +1,18 @@
+public class TaskManager {
+
+    public void markTaskAsCompleted(String task) {
+
+    }
+
+    public void displayAllTasks() {
+
+    }
+
+    public void displayIncompleteTasks() {
+
+    }
+
+    public void displayCompletedTasks() {
+
+    }
+}
