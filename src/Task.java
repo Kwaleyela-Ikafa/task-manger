ecef3c9ddde18db7558e90259d72524cec5438b9
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2024-05-20 17:37:25 +0200

    Create getters and setters

--- src/Task.java
+++ src/Task.java
@@ -2,5 +2,52 @@ public class Task {
     private String title;
     private String description;
     private String due_date;
-    private String completion_status;
+    private Boolean completed;
+
+    public Task(String title, String description, String due_date) {
+        setTitle(title);
+        setDescription(description);
+        setDue_date(due_date);
+        this.completed = false;
+    }
+
+    public Task(Task source) {
+        setTitle(source.title);
+        setDescription(source.description);
+        setDue_date(source.due_date);
+    }
+
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDue_date() {
+        return this.due_date;
+    }
+
+    public void setDue_date(String due_date) {
+        this.due_date = due_date;
+    }
+
+    public Boolean isCompleted() {
+        return this.completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
 }
